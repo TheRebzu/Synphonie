e00189c496208f47d30bfa0a88fd69b8222ab57e
Rebzu <adda.amine09@gmail.com>
2024-07-15 00:43:13 +0200

    Initial commit for final project

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,38 @@
+<?php
+
+// src/Controller/ProductController.php
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/products", name="get_products", methods={"GET"})
+     */
+    public function getProducts(ProductRepository $productRepository): JsonResponse
+    {
+        $products = $productRepository->findAll();
+        return $this->json($products);
+    }
+
+    /**
+     * @Route("/products", name="create_product", methods={"POST"})
+     */
+    public function createProduct(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $product = new Product();
+        $product->setName($data['name']);
+        $product->setPrice($data['price']);
+        $em->persist($product);
+        $em->flush();
+        return $this->json($product);
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+// src/Entity/Product.php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    // getters and setters
+}
